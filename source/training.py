c7c1f8a1904737a679ad1c580f4fdba15039b4c1
Wilson Zhou <wilsonzhou92@gmail.com>
2021-02-28 02:24:30 +0100

    training seasonality

--- source/training.py
+++ source/training.py
@@ -139,19 +139,29 @@ class StockProcess:
         return dataSplit
             
 
-class Featues:
+class Features:
     
     def __init__(self, date: date, values: List[float]):
         self.date   = date
         self.values = values
         
+    def prepareForPd(self):
+        
+        featuresWithCurrentDate = {'date': self.date}
+        for i, featureValue in enumerate(self.values):
+            featuresWithCurrentDate[i] = featureValue
+            
+        return featuresWithCurrentDate
+        
 class Measurement:
     
     def __init__(self, date: date, value: float):
         self.date   = date
-        self.value = value
+        self.value  = value
         
+    def prepareForPd(self):
         
+        return {'date': self.date, 'label': self.value}
         
 class TrainingData:
     
@@ -159,14 +169,26 @@ class TrainingData:
         
         self.stockName       = stockProcess.stockName
         self.stockPrices     = stockProcess.stockPrices
-        self.currAndFuture   = stockProcess.splitCurrPriceAndNextTDays(t = t)
-        self.currAndPast     = stockProcess.splitCurrPriceAndPastKDays(k = k)
         
-        self.nFutureDaysMoreExpensiveThanCurrentPrice = self.countDaysMoreExpensiveThanCurrent(self.currAndFuture, p = p)
-        self.nPastDaysMoreExpensiveThanCurrentPrice   = self.countDaysMoreExpensiveThanCurrent(self.currAndPast, p = p)
+        # Future
+        currAndFuture   = stockProcess.splitCurrPriceAndNextTDays(t = t)        
+        self.nFutureDaysMoreExpensiveThanCurrentPrice = self.countDaysMoreExpensiveThanCurrent(currAndFuture, p = p)
+        self.futurePrices    = self.comparedPricesPerCurrentDate(currAndFuture)
         
-        self.futurePrices = self.comparedPricesPerCurrentDate(self.currAndFuture)
-        self.pastPrices   = self.comparedPricesPerCurrentDate(self.currAndPast)
+        # Past
+        currAndPast     = stockProcess.splitCurrPriceAndPastKDays(k = k)
+        self.nPastDaysMoreExpensiveThanCurrentPrice   = self.countDaysMoreExpensiveThanCurrent(currAndPast, p = p)
+        self.pastPrices      = self.comparedPricesPerCurrentDate(currAndPast)
+        self.featuresForCurrentPrice = self.splitCurrentEntityAndPast(self.nPastDaysMoreExpensiveThanCurrentPrice, k = k)
+    
+        # For each date, join the past (features) and the future (label)
+        dataframe          = self.joinFeaturesAndLabels()
+        self.labeledData   = dataframe.query("label.notna()")
+        self.unlabeledData = dataframe.query("label.isna()")
+        
+        # split into features (X) and labels (y)
+        self.X = self.labeledData.drop(columns = ['label']).copy()
+        self.y = self.labeledData.label.copy()
     
     def countDaysMoreExpensiveThanCurrent(self, dataPairList: List[DataSplit], p: float) -> List[Measurement]:
                 
@@ -179,42 +201,26 @@ class TrainingData:
             
         return measurements
             
-        
-        
-    
-    def pctPriceChange(self, dataPairList: List[CurrPriceAndPastPrices]):
-        
-        features = list()
-        
-        for dataPair in dataPairList:
-            
-            featureValues = pctChange([x.price for x in datasplit.compared] + [datasplit.current.price])    
-            features.append(Feature(dataPair.current.date, featureValues))
-            
-        return features
-            
             
         
-        #self.featuresFromPast= [[datasplit.current.date] + pctChange([x.price for x in datasplit.compared] + [datasplit.current.price]) for datasplit in self.currAndPast]
-        #profitDaysPastCount= [(datasplit.current.date, datasplit.countDaysMoreExpensiveThanCurrentPricePlusPct(p = p)) for datasplit in self.currAndPast]
+    def splitCurrentEntityAndPast(self, measurements: List[Measurement], k: int) -> List[Features]:
         
-        
-        
-    def self.nPastDaysMoreExpensiveThanCurrentPrice
-        self.featuresFromPast = list()
         i = 0
+        pastMeasurements = list()
         
-        for i in range(len(profitDaysPastCount)):
+        for i in range(len(measurements)):
                        
             if i >= k:
                 
-                currentDate, currentCount = profitDaysPastCount[i]
-                pastCounts                = [x[1] for x in profitDaysPastCount[i - k: i]]
+                currentDate  = measurements[i].date
+                pastCounts   = [x.value for x in measurements[i - k + 1: i + 1]]
                 
-                self.featuresFromPast.append([currentDate] + pastCounts)
+                pastMeasurements.append(Features(currentDate, pastCounts))
                     
             
             i =+ 1
+            
+        return pastMeasurements
         
         
     def comparedPricesPerCurrentDate(self, dataPairList: List[DataSplit]):
@@ -227,6 +233,27 @@ class TrainingData:
             comparedPrices[currentDate] = [x.price for x in dataPair.compared]
 
         return comparedPrices
+    
+    def joinFeaturesAndLabels(self):
+        
+        labelsDf   = pd.DataFrame([m.prepareForPd() for m in self.nFutureDaysMoreExpensiveThanCurrentPrice])
+        featuresDf = pd.DataFrame([f.prepareForPd() for f in self.featuresForCurrentPrice])
+        
+        finalDf = featuresDf.merge(labelsDf, on = 'date', how = 'left').set_index('date')
+        
+        return finalDf
+    
+    def pctPriceChange(self, dataPairList: List[CurrPriceAndPastPrices]):
+        
+        features = list()
+        
+        for dataPair in dataPairList:
+            
+            featureValues = pctChange([x.price for x in dataPair.compared] + [dataPair.current.price])    
+            features.append(Features(dataPair.current.date, featureValues))
+            
+        return features
+            
 
 
 stockName = 'NOVO-B.CO'
@@ -241,31 +268,22 @@ stockPrices = [StockPrice(*s) for s in data.values]
 sp = StockProcess(stockName, stockPrices)
 td = TrainingData(sp, 30, 30, 0.02)
 
-td.featuresFromPast
-
-
-# Split data
-
-
 
 # Training
-labelsDf   = pd.DataFrame(td.profitDaysCount).rename(columns = {0: 'date', 1: 'label'})
-featuresDf = pd.DataFrame(td.featuresFromPast).rename(columns = {0: 'date'})
+X = td.X.copy()
+# X['season'] = X.index.map(lambda x: x.month).astype(str)
+from sklearn.preprocessing import OneHotEncoder
+ohe = OneHotEncoder(drop = 'first')
+months = X.index.map(lambda x: x.month).values.reshape(-1 ,1)
+ohe.fit(months)
+season = pd.DataFrame(ohe.transform(months).toarray(), index = X.index)
+y = td.y.copy()
 
-finalDf = featuresDf.merge(labelsDf, on = 'date', how = 'left').set_index('date').query("label.notna()")
-
-X = finalDf.drop(columns = ['label']).copy()
-y = finalDf.label.copy()
-
-
-# X_train = X.copy().iloc[:-30]
-# y_train = y.copy().iloc[:-30]
-# X_test  = X.copy().iloc[-30:]
-# y_test  = y.copy().iloc[-30:]
-
+# Split data
 from sklearn.model_selection import train_test_split# Split the data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 42)
 
+# linear regression
 from sklearn.linear_model import LinearRegression
 lr = LinearRegression()
 lr.fit(X_train, y_train)
@@ -283,9 +301,10 @@ y_pred_rf = rf.predict(X_test)
 
 
 summaryPred = pd.DataFrame(zip(y_pred_lr, y_pred_rf, y_test), index = y_test.index).round().merge(pd.DataFrame([p.__dict__ for p in td.stockPrices]).set_index('date'), left_index = True, right_index = True)
-summaryPred['future'] = summaryPred.index.map(td.pricesFromFuture.get)
+summaryPred['future'] = summaryPred.index.map(td.futurePrices.get)
 summaryPred['past']  = list(X_test.values)
 
+summaryPred.to_csv('temp.csv', sep = ';')
 
 
 
